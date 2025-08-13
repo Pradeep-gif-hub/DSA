182f7407fad1cdeddaa522ea2cd143b886356181
Pradeep Awasthi <pawasthi063@gmail.com>
2025-08-13 23:19:04 +0530

    Maxnums

--- maxcons1s.cpp
+++ maxcons1s.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
-int maxones(vector<int>&arr){
-    for(int i=0;i<arr.size();i++){
-        int len=0;
-        while(arr[i]==1 && arr[i]!=arr.end()){
-            len+=1;
-            continue;
-        }
-        return len;
 
+int maxones(vector<int>& arr) {
+    int maxCount = 0, count = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == 1) {
+            count++;
+            maxCount = max(maxCount, count);
+        } else {
+            count = 0;
+        }
     }
+    return maxCount;
 }
-int main(){
-    vector<int>arr={1,1,1,1,0,0,1,1,0,1,1,1,1,0,3,4};
-    return maxones(arr);
+
+int main() {
+    vector<int> arr = {1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 3, 4};
+    cout << "Maximum consecutive 1s: " << maxones(arr) << endl;
     return 0;
 }
