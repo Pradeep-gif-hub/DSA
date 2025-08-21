49ad4873debb953c975ca62fbf6cc9eaf627d119
Pradeep Awasthi <pawasthi063@gmail.com>
2025-08-22 01:59:32 +0530

    Stacks and Queue impelmentation

---
+++ stackfromlinkedlist.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node* back;
+
+    Node(int data1, Node* next1=nullptr, Node* back1=nullptr){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+};
+
+Node* conversion(vector<int>& arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        prev->next = temp;
+        temp->back = prev;
+        prev = temp;
+    }
+    return head;
+}
+
+vector<int> stackfromlinkedlist(Node* head){
+    stack<int> st;
+    vector<int> ans;
+    Node* it = head;
+    while(it != nullptr){
+        st.push(it->data);
+        it = it->next;
+    }
+    while(!st.empty()){
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {2,3,4,5,6,7,8,9,1};
+    Node* head = conversion(arr);
+    vector<int> ans = stackfromlinkedlist(head);
+
+    for(int x : ans) cout << x << " ";
+    return 0;
+}
---
+++ validparenthesis.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isvalidparenthesis(string s){
+    stack<char> st;
+    for(char c : s){
+        if(c == '(' || c == '{' || c == '['){
+            st.push(c);
+        } 
+        else {
+            if(st.empty()) return false;  
+            char top = st.top();
+            st.pop();
+            if((c == ')' && top != '(') ||
+               (c == '}' && top != '{') ||
+               (c == ']' && top != '[')){
+                return false;
+            }
+        }
+    }
+    return st.empty(); // if the st is empty
+}
+
+int main(){
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+    cout << (isvalidparenthesis(s) ? "Valid" : "Invalid");
+    return 0;
+}
